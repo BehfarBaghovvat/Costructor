b4a5c84ba1bf12bcbf09120a7b1117f57fc4290d
Behfar Baghovvat <behfar.baghovvat@gmail.com>
2019-02-05 23:40:16 +0330

    Add project files.

---
+++ Costructor/Person.cs
@@ -0,0 +1,76 @@
+﻿namespace Costructor
+{
+
+    /// <summary>
+    /// Person calss    
+    /// </summary>
+
+    class Person
+    {        
+        public int Age;
+        public string FullName;       
+
+        public static int age;
+        public static string fullname;
+
+        /// <summary>
+        /// Show informatio person.
+        /// </summary>
+
+        public void Information()
+        {
+
+            string Result = $"Full Name: {FullName} and {Age} years old";
+            System.Console.WriteLine(Result);
+            System.Console.WriteLine();
+        }
+
+        /// <summary>
+        ///Default constructor
+        /// </summary>
+
+        public Person()
+        {
+        }
+
+        //-----Constructor overload-----
+
+        /// <summary>
+        /// Person class constructor
+        /// It is a Constructor whose only field Age and Full Name must be set.
+        /// </summary>
+        /// <param name="age">Person's Age</param>
+        /// <param name="fullname">Person's Full Name</param>
+
+        public Person(int age, string fullname)
+        {
+            Age = age;
+            FullName = fullname;
+        }
+
+        /// <summary>
+        /// Person class constructor
+        /// It is a Constructor whose only field Age must be set.
+        /// </summary>
+        /// <param name="age">Person's age</param>
+
+        public Person(int age)
+        {
+            Age = age;
+
+        }
+
+        /// <summary>
+        /// Person class constructor
+        /// It is a Constructor whose only field Full Name must be set.
+        /// </summary>
+        /// <param name="fullname">Person's Full Name</param>
+
+        public Person(string fullname)
+        {
+            FullName = fullname;
+        }
+
+        
+    }
+}
---
+++ Costructor/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace Costructor
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+
+            //==================================================
+            Person p1 = new Person();
+
+            p1.Age = 20;
+            p1.FullName = "Reza Karimi";
+
+            p1.Information();
+
+            //==================================================
+            //==================================================
+
+            Person p2 = new Person() { Age = 30, FullName = "Mona Ahmadi", };
+
+            p2.Information();
+
+            //==================================================
+            //==================================================
+
+            Person p3 =
+                new Person()
+                {
+                    FullName = "Behfar Baghovvat",
+
+                    Age = 36,
+
+                };           
+
+            p3.Information();
+
+            //==================================================
+            //==================================================
+
+            Person p4 =
+                new Person
+                {
+
+                    Age=25,
+                    
+                    FullName="Zahra Aslani",
+
+                };
+            
+            p4.Information();
+
+            //==================================================
+            //==================================================
+            //==The best setting for CONSTRUCTOR is as follows==
+
+            Person p5 = 
+                new Person(age:40,fullname:"Kamran Ebadi");
+
+            p5.Information();
+
+            //==================================================
+            //==================================================
+
+
+
+
+            System.Console.ReadLine();
+        }
+    }
+}
